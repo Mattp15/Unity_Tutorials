49bfbb31aa53dccb8e03c38aa041d513819ba60f
Mattp15 <Matthewrphillips15@gmail.com>
2023-02-07 22:42:41 -0500

    added input control

--- Obsticle Course/Assets/Mover.cs
+++ Obsticle Course/Assets/Mover.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     //SeralizeField gives Unity the access to the variables to change in app, however chaning them in unity interface will not change them in the script
-    [SerializeField] float xValue = 0.01f;
     [SerializeField] float yValue = 0.0f;
-    [SerializeField] float zValue = 0.0f;
-    int count = 0;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +16,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        count++;
-        if(count >= 1000)
-        {
-            xValue = -xValue;
-            count = 0;
-        } 
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
         transform.Translate(xValue, yValue, zValue);
-
     }
 }
