57469afbfba439bd7c10097aeb399113553b5910
Mattp15 <Matthewrphillips15@gmail.com>
2023-02-09 17:52:19 -0500

    refactored rotaion into a method

--- Rocket/Assets/Scripts/Movement.cs
+++ Rocket/Assets/Scripts/Movement.cs
@@ -7,6 +7,7 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     Rigidbody rb; 
     [SerializeField]float mainThrust = 1000f;
+    [SerializeField] float rotateThrust = 100f;
 
     void Start()
     {
@@ -35,11 +36,15 @@ public class Movement : MonoBehaviour
     {
     if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            ApplyRotation(-rotateThrust);
         } 
         else if(Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            ApplyRotation(rotateThrust);
         }
     }
-}
+    void ApplyRotation(float rotationThisFrame)
+    {
+    transform.Rotate(-Vector3.forward * rotationThisFrame * Time.deltaTime);
+    }
+    }
