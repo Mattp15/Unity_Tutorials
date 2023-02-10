b3fc8c404148f6a7f1aababa2204c74d4f7463cb
Mattp15 <Matthewrphillips15@gmail.com>
2023-02-09 19:32:18 -0500

    adding the scene functionality to reset or go to next

---
+++ Rocket/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    
+    private void OnCollisionEnter(Collision other) 
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Finish":
+                LoadNextLevel();
+                break;
+            case "Friendly":
+                break;
+            default:
+                ReloadLevel();
+                break;
+        }    
+    }
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+    
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }        
+        SceneManager.LoadScene(nextSceneIndex);
+
+    }
+}
