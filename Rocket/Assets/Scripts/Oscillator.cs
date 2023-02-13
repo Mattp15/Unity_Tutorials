211488a9048db4e5c3a0c1f2a81c295b2f7680b6
Mattp15 <Matthewrphillips15@gmail.com>
2023-02-13 13:50:45 -0500

    added notes about how the script works - need more on Sin

--- Rocket/Assets/Scripts/Oscillator.cs
+++ Rocket/Assets/Scripts/Oscillator.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour
 {
-    Vector3 startingPosition;
+    [SerializeField] Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    float movementFactor; //determins the movement distance
     [SerializeField] float period = 10f;
     // Start is called before the first frame update
     void Start()
     {
         startingPosition = transform.position;
+        movementFactor = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        const float tau = Mathf.PI * 2;
+        const float tau = Mathf.PI * 2;//const value of double pie 6.283 apparently better for circles
         float cycles = Time.time / period;
-        float rawSinWage = Mathf.Sin(cycles * tau);
+        float rawSinWave = Mathf.Sin(cycles * tau); //TextGenerationSettings from -1 to 1
+
+        movementFactor = (rawSinWave + 1) /2f; //recalculated to go from 0 to 1 so its "cleaner"
 
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
