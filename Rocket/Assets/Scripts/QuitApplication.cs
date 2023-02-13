5d378300606157ac5b38de8c0f2471efdffb4ed8
Mattp15 <Matthewrphillips15@gmail.com>
2023-02-13 14:31:53 -0500

    Added ability to quit game

---
+++ Rocket/Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("quitting");
+            Application.Quit(); //This won't show anything while compiling in unity
+        }
+    }
+}
