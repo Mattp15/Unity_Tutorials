0502f01e677279fbfe31e71554d9f07cca3a7678
Mattp15 <Matthewrphillips15@gmail.com>
2023-02-13 10:46:39 -0500

    refactored functions to be more readable

--- Rocket/Assets/Scripts/Movement.cs
+++ Rocket/Assets/Scripts/Movement.cs
@@ -34,51 +34,76 @@ public class Movement : MonoBehaviour
     if (Input.GetKey(KeyCode.Space))
         {
             //Check out vector shortcuts up = 0, 1, 0
-            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
-
-            if(!audioSource.isPlaying)
-            {
-                audioSource.PlayOneShot(mainEngine);
-            } 
-            if(!thrustParticles.isPlaying)
-            {
-                thrustParticles.Play();
-            }
-        } 
+            StartThrusting();
+        }
         else
         {
-            audioSource.Stop();
-            thrustParticles.Stop();
+            StopThrusting();
         }
     }
 
+    private void StopThrusting()
+    {
+        audioSource.Stop();
+        thrustParticles.Stop();
+    }
+
+    private void StartThrusting()
+    {
+        rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        if (!thrustParticles.isPlaying)
+        {
+            thrustParticles.Play();
+        }
+    }
 
     void ProcessRotation()
     {
     if (Input.GetKey(KeyCode.A))
         {
-            ApplyRotation(-rotateThrust);
-            rightParticles.Play();
-        } 
+            TurningLeft();
+        }
         else if(Input.GetKey(KeyCode.D))
         {
-            ApplyRotation(rotateThrust);
-            if(!leftParticles.isPlaying)
-            {
-                leftParticles.Play();
-            }     
+            TurningRight();
         }
-        else 
+        else
         {
-            rightParticles.Stop();
-            leftParticles.Stop();
+            StopTurningParticles();
         }
 
     }
+
+    private void StopTurningParticles()
+    {
+        rightParticles.Stop();
+        leftParticles.Stop();
+    }
+
+    private void TurningRight()
+    {
+        ApplyRotation(rotateThrust);
+        if (!leftParticles.isPlaying)
+        {
+            leftParticles.Play();
+        }
+    }
+
+    private void TurningLeft()
+    {
+        ApplyRotation(-rotateThrust);
+        rightParticles.Play();
+    }
+
     void ApplyRotation(float rotationThisFrame)
     {
         rb.freezeRotation = true; //freezing rotation so we can manually rotate
-    transform.Rotate(-Vector3.forward * rotationThisFrame * Time.deltaTime);
-    rb.freezeRotation = false;
+        transform.Rotate(-Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false;
     }
     }
