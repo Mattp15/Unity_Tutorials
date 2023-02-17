6e2acdd197dacc0642a9bd223fa86fdfe4dd4f9c
Mattp15 <Matthewrphillips15@gmail.com>
2023-02-17 17:46:16 -0500

    getting input with 'old method'

--- Argon_Assult/Assets/Scripts/PlayerController.cs
+++ Argon_Assult/Assets/Scripts/PlayerController.cs
@@ -13,6 +13,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalAxis = Input.GetAxis("Horizontal");
+        float horizontalThrow = Input.GetAxis("Horizontal");
+        Debug.Log(horizontalThrow);
+        float verticalThrow = Input.GetAxis("Vertical");
+        Debug.Log(verticalThrow);
     }
 }
