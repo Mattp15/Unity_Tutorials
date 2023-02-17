ad8a9c29c749a3ab7ecf783b59e9418a49d8f1fc
Mattp15 <Matthewrphillips15@gmail.com>
2023-02-17 18:09:39 -0500

    added and imported Inputsystem

--- Argon_Assult/Assets/Scripts/PlayerController.cs
+++ Argon_Assult/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] InputAction movement;
     void Start()
     {
         
@@ -13,9 +15,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        Debug.Log(horizontalThrow);
-        float verticalThrow = Input.GetAxis("Vertical");
-        Debug.Log(verticalThrow);
+        // float horizontalThrow = Input.GetAxis("Horizontal");
+        // Debug.Log(horizontalThrow);
+        // float verticalThrow = Input.GetAxis("Vertical");
+        // Debug.Log(verticalThrow);
+
+
     }
 }
