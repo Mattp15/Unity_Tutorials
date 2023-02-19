e3b6a835a26b792fe9a7c8842a7ac42f3bc2a6d5
Mattp15 <Matthewrphillips15@gmail.com>
2023-02-19 09:44:45 -0500

    added rotation control

--- Argon_Assult/Assets/Scripts/PlayerController.cs
+++ Argon_Assult/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,13 @@ public class PlayerController : MonoBehaviour
     float xRange = 10f;
     float yRange = 7f;
     float controlSpeed = 8.8f;
+    float xThrow, yThrow;
+    [SerializeField] float positionPitchFactor = -2.5f;
+    [SerializeField] float controlPitchFactor = -10f;
+    [SerializeField] float postiionRollFactor = -2f;
+
+    [SerializeField] private float rotationFactor;
+
     void Start()
     {
         
@@ -24,13 +31,31 @@ public class PlayerController : MonoBehaviour
     }
     void Update()
     {
+        processTranslation();
+        processRotation();
+
+    }
+    private void processRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControllThrow = yThrow * controlPitchFactor;
+        
+        float pitch = pitchDueToPosition * pitchDueToControllThrow;         
+        float yaw = 0f;
+        float roll = transform.localPosition.x * postiionRollFactor + xThrow * controlPitchFactor;
+        Quaternion targetRotation = Quaternion.Euler(pitch, yaw, roll);
+        transform.localRotation = Quaternion.RotateTowards(transform.localRotation, targetRotation, rotationFactor);
+    }
+
+    private void processTranslation()
+    {
         //BELOW IS OLD SYSTEM
         // float horizontalThrow = Input.GetAxis("Horizontal");
         // float verticalThrow = Input.GetAxis("Vertical");
 
         //Below is the new system
-        float xThrow = movement.ReadValue<Vector2>().x;//.x impliments horizontal axis (x-axis)
-        float yThrow = movement.ReadValue<Vector2>().y;//.y impliments vertical axis (y-axis)
+        xThrow = movement.ReadValue<Vector2>().x;//.x impliments horizontal axis (x-axis)
+        yThrow = movement.ReadValue<Vector2>().y;//.y impliments vertical axis (y-axis)
         //Get X movement
         float xOffset = xThrow * Time.deltaTime * controlSpeed;
         float rawXPos = transform.localPosition.x + xOffset;
@@ -42,9 +67,5 @@ public class PlayerController : MonoBehaviour
 
         float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
-        
-       
-
-
     }
 }
