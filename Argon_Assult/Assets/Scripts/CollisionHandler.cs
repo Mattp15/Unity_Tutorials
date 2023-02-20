800f225ece8cb91765fc22df69bbda38c25bd544
Mattp15 <Matthewrphillips15@gmail.com>
2023-02-20 17:06:09 -0500

    Added script to prefab ship and rigid body

--- Argon_Assult/Assets/Scripts/CollisionHandler.cs
+++ Argon_Assult/Assets/Scripts/CollisionHandler.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-    
+    private void OnCollisionEnter(Collision other) 
+    {
+        Debug.Log($"{this.name} --Collided with-- {other.gameObject.name}");//don't need this. in this instance
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        Debug.Log($"{this.name} **Triggered by** {other.gameObject.name}");    
+    }
 }
